9f31926010185fb4dcf99c60ca3fdf7462f53f98
Ibby Hadeed <ibby93@gmail.com>
2017-07-05 13:52:37 -0400

    use submodule instead of sister directory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
-import { TooltipsModule } from '../../../ionic-tooltips/src';
+import { TooltipsModule } from '../ionic-tooltips/src';
 
 @NgModule({
   declarations: [
