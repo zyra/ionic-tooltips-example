996f3eee59aea361730e77b90ebdb8b36aef55bf
Ibby Hadeed <ibby@zyra.ca>
2018-09-23 20:25:02 -0400

    Upgrade project dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
+import { BrowserModule }                            from '@angular/platform-browser';
+import { ErrorHandler, NgModule }                   from '@angular/core';
+import { BrowserAnimationsModule }                  from '@angular/platform-browser/animations';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen }                             from '@ionic-native/splash-screen';
+import { StatusBar }                                from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -16,6 +17,7 @@ import { TooltipsModule } from '../ionic-tooltips/src';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     IonicModule.forRoot(MyApp),
     TooltipsModule
   ],
